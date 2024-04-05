fdcbf3fd0d86c4550a9f28c7f907bcb185922bc5
Michal <mspytlos@gmail.com>
2024-04-05 22:08:56 +0200

    Fix formatting

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -6,7 +6,22 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter/issues/3970#issuecomment-1377126359
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "bash", "sql", "yaml", "toml", "json", "dockerfile", "markdown", "markdown_inline"},
+  ensure_installed = {
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "python",
+    "bash",
+    "sql",
+    "yaml",
+    "toml",
+    "json",
+    "dockerfile",
+    "markdown",
+    "markdown_inline",
+  },
   highlight = { enable = true },
   indent = { enable = true }
 }
